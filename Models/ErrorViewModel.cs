88f9575502242541097c5c3a85b49f1aa05ea31b
Musayyab Ali <Musayyabali@crunchcraft.com>
2024-05-30 11:19:19 +0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppWithCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
